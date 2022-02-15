f8d6962b6d0544eef7af1640641e316fe0d0b9b8
Jen Caza <jencaza@gmail.com>
2022-02-14 20:32:22 -0500

    cards display address and phone number

--- client/src/components/PlaceDetails/PlaceDetails.jsx
+++ client/src/components/PlaceDetails/PlaceDetails.jsx
@@ -35,6 +35,16 @@ const PlaceDetails = ({place}) => {
         {place?.cuisine?.map(({ name }) => (
           <Chip key={name} size="small" label={name} className={classes.chip}/>
         ))}
+        {place.address && (
+          <Typography gutterBottom variant="body2" color="textSecondary" className={classes.subtitle}>
+            <LocationOnIcon />{place.address}
+          </Typography>
+        )}
+        {place.phone && (
+          <Typography variant="body2" color="textSecondary" className={classes.spacing}>
+            <PhoneIcon /> {place.phone}
+          </Typography>
+        )}
       </CardContent>
     </Card>
   );
