cf97e74aa3a8045392de6fa4cb12b1d8f923eadb
Zakiya <zakiya@Zakiyas-MacBook-Air.local>
2022-02-18 19:03:39 -0500

    Add token to new user

---
+++ server/utils/jwt.js
@@ -0,0 +1,24 @@
+const jwt = require('jsonwebtoken');
+const JWT_SECRET_KEY= '12345';
+const generateToken = function(user){
+var token = jwt.sign({ userId: user.user_id }, JWT_SECRET_KEY, { expiresIn: '1d'});
+return token;
+}
+
+// module.exports= {generateToken}
+
+const decodeToken = function(token){
+    try {
+        const decodedToken = jwt.verify(token, JWT_SECRET_KEY );
+      return decodedToken.userId;
+      } catch (error) {
+        console.error(error);
+        return null;
+      }
+}
+
+
+
+
+
+module.exports= {generateToken, decodeToken}
