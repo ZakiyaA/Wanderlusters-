4a2ab2c8523da47763ac7be72582509e68c70f34
Khadeeja <kkausar59@gmail.com>
2022-02-21 20:14:14 -0500

    Precise rating

--- client/src/components/PlaceDetails/PlaceDetails.jsx
+++ client/src/components/PlaceDetails/PlaceDetails.jsx
@@ -1,5 +1,14 @@
-import React from 'react';
-import { Box, Typography, Button, Card, CardMedia, CardContent, CardActions, Chip} from "@material-ui/core";
+import React from "react";
+import {
+  Box,
+  Typography,
+  Button,
+  Card,
+  CardMedia,
+  CardContent,
+  CardActions,
+  Chip,
+} from "@material-ui/core";
 import LocationOnIcon from "@material-ui/icons/LocationOn";
 import PhoneIcon from "@material-ui/icons/Phone";
 import { Rating } from "@material-ui/lab";
@@ -8,61 +17,103 @@ import useStyles from "./styles";
 const PlaceDetails = ({ place, selected, refProp }) => {
   const classes = useStyles();
 
-  if(selected) {
-    refProp?.current?.scrollIntoView({ behaviour: 'smooth', block: 'start' })
-  } 
-  
-  return(
+  if (selected) {
+    refProp?.current?.scrollIntoView({ behaviour: "smooth", block: "start" });
+  }
+
+  return (
     <Card ref={refProp} elevation={6}>
-      <CardMedia 
-          style={{ height: 350 }}
-          image={place.photo ? place.photo.images.large.url : 'https://www.foodserviceandhospitality.com/wp-content/uploads/2016/09/Restaurant-Placeholder-001.jpg'}
-          title={place.name}
+      <CardMedia
+        style={{ height: 350 }}
+        image={
+          place.photo
+            ? place.photo.images.large.url
+            : "https://www.foodserviceandhospitality.com/wp-content/uploads/2016/09/Restaurant-Placeholder-001.jpg"
+        }
+        title={place.name}
       />
       <CardContent>
-        <Typography gutterBottom variant="h5">{place.name}</Typography>
+        <Typography gutterBottom variant="h5">
+          {place.name}
+        </Typography>
         <Box display="flex" justifyContent="space-between">
-        <Rating size="medium" value={Number(place.rating)} readOnly ></Rating>
-          <Typography gutterBottom variant="subtitle1">out of {place.num_reviews} reviews</Typography>
+          <Rating
+            size="medium"
+            value={Number(place.rating)}
+            precision
+            readOnly
+          ></Rating>
+          <Typography gutterBottom variant="subtitle1">
+            out of {place.num_reviews} reviews
+          </Typography>
         </Box>
         <Box display="flex" justifyContent="space-between">
           <Typography variant="subtitle1">Price</Typography>
-          <Typography gutterBottom variant="subtitle1">{place.price_level}</Typography>
+          <Typography gutterBottom variant="subtitle1">
+            {place.price_level}
+          </Typography>
         </Box>
         <Box display="flex" justifyContent="space-between">
           <Typography variant="subtitle1">Ranking</Typography>
-          <Typography gutterBottom variant="subtitle1">{place.ranking}</Typography>
+          <Typography gutterBottom variant="subtitle1">
+            {place.ranking}
+          </Typography>
         </Box>
         {place?.awards?.map((award) => (
-          <Box key={award.display_name} display="flex" justifyContent="space-between" alignItems="center">
-              <img src={award.images.small} alt="award.display_name" />
-              <Typography variant="subtitle2" color="textSecondary">{award.display_name}</Typography>
+          <Box
+            key={award.display_name}
+            display="flex"
+            justifyContent="space-between"
+            alignItems="center"
+          >
+            <img src={award.images.small} alt="award.display_name" />
+            <Typography variant="subtitle2" color="textSecondary">
+              {award.display_name}
+            </Typography>
           </Box>
         ))}
         {place?.cuisine?.map(({ name }) => (
-          <Chip key={name} size="small" label={name} className={classes.chip}/>
+          <Chip key={name} size="small" label={name} className={classes.chip} />
         ))}
         {place.address && (
-          <Typography gutterBottom variant="body2" color="textSecondary" className={classes.subtitle}>
-            <LocationOnIcon />{place.address}
+          <Typography
+            gutterBottom
+            variant="body2"
+            color="textSecondary"
+            className={classes.subtitle}
+          >
+            <LocationOnIcon />
+            {place.address}
           </Typography>
         )}
         {place.phone && (
-          <Typography variant="body2" color="textSecondary" className={classes.spacing}>
+          <Typography
+            variant="body2"
+            color="textSecondary"
+            className={classes.spacing}
+          >
             <PhoneIcon /> {place.phone}
           </Typography>
         )}
       </CardContent>
       <CardActions>
-        <Button size="small" color="primary" onClick={() => window.open(place.web_url, '_blank')}>
+        <Button
+          size="small"
+          color="primary"
+          onClick={() => window.open(place.web_url, "_blank")}
+        >
           Trip Advisor
         </Button>
-        <Button size="small" color="primary" onClick={() => window.open(place.website, '_blank')}>
+        <Button
+          size="small"
+          color="primary"
+          onClick={() => window.open(place.website, "_blank")}
+        >
           Website
         </Button>
       </CardActions>
     </Card>
   );
-}
+};
 
 export default PlaceDetails;
